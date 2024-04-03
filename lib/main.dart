a365f3528d5caad1b0a5d8d841b78efcba6fd4ce
Abuessa265 <abuessastudent1999@gmail.com>
2024-04-03 10:32:20 +0600

    add cupertino design language app

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,44 @@
-//Navigation -push,pop,replace,removeUntil(without named navigation)
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_sourch_code_flow/home.dart';//its package import
-//import 'home.dart'; //its path import
 
-
-main() {
-  runApp(MyApp());
+void main() {
+  runApp(iOSStyleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class iOSStyleApp extends StatelessWidget {
+  const iOSStyleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Home'),
+          trailing: CupertinoButton(
+            onPressed: () {},
+            child: Icon(CupertinoIcons.add),
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoActivityIndicator(
+                radius: 25,
+              ),
+              CupertinoSwitch(value: false, onChanged: (value){})
+            ],
+          ),
+        ));
+  }
+}
