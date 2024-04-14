4cbda6b3f9ee9a948635e77352c4ac775dbe0e14
Abuessa265 <abuessastudent1999@gmail.com>
2024-04-14 09:15:31 +0600

    statefulWidget and its life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,58 @@
-//stateful widget introduction
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'splash.dart';
 
-void main(){
-  runApp(myApp());
+void main() {
+  runApp(const MyApp());
 }
-class myApp extends StatelessWidget{
-  const myApp({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomeActivity());
+      home: const Splash(),
+      theme: ThemeData(),
+    );
   }
 }
 
-class HomeActivity extends StatelessWidget{
-   HomeActivity({super.key});
+/*
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-
-  int counter=0;
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text('Home'),
-       leading: Icon(Icons.home),
-       actions: [
-         Icon(Icons.add),
-       ],
-     ),
-     body: Center(
-       child: Text(counter.toString(),style: TextStyle(
-         fontSize: 40,
-       ),),
-     ),
-     floatingActionButton: FloatingActionButton(
-       onPressed: (){
-         counter++;
-         print(counter);
-       },
-       child: Icon(Icons.add),
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        leading: Icon(Icons.home),
+        actions: [
+          Icon(Icons.add)
+        ],
+      ),
+      body: Center(
+        child: Text(counter.toString(), style: TextStyle(
+          fontSize: 48
+        ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter++;
+          print(counter);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
+}*/
 
-}
+
+
+
+// Widget - 2 types
+// Stateless Widget
+// Stateful Widget
+// State
