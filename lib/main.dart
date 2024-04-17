c1a9d35ecce3309f4995f1e8e547c55114630fb0
Abuessa265 <abuessastudent1999@gmail.com>
2024-04-17 09:52:19 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,44 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //MediaQuery এর মাধ্যমে ডিভাইস এর স্ক্রিন এর সাইজ,ওয়িথ অরিয়েন্টেশন ইত্যাদি জানা জাই
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).orientation);
+    print(MediaQuery.of(context).devicePixelRatio);
+    print(MediaQuery.of(context).displayFeatures);
+
+    print(MediaQuery.displayFeaturesOf(context));
+    print(MediaQuery.sizeOf(context));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
+        centerTitle: true,
+        title: const Text('Home'),
+      ),
+      body: Center(
+        //wrap এটি row এর মোত কাজ করে কিন্তু row সেম লাইন এ জাইগা শেষ হলে নিচের লাইন থেকে স্টার্ট করতে পারে না কিন্তু wrap এটি পারে
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          spacing: 10,
+          children: [
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+          ],
+        ),
       ),
     );
   }
