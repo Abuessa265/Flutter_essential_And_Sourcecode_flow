3e5c01a6aa5186179bf66dd849d874a08151717b
Abuessa265 <abuessastudent1999@gmail.com>
2024-04-18 08:26:24 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -38,31 +38,35 @@ class Home extends StatelessWidget {
         title: const Text('Home'),
       ),
 
+      /*
       //wrap এটি row এর মতো কাজ করে কিন্তু row সেম লাইন এ জাইগা শেষ হলে নিচের লাইন থেকে স্টার্ট করতে পারে না কিন্তু wrap এটি পারে
-      // body: Center(
-      //   child: Wrap(
-      //     alignment: WrapAlignment.center,
-      //     crossAxisAlignment: WrapCrossAlignment.start,
-      //     spacing: 10,
-      //     children: [
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //     ],
-      //   ),
-      // ),
+      body: Center(
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          spacing: 10,
+          children: [
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+          ],
+        ),
+      ),
+       */
+      /*
       body: LayoutBuilder(
+        //LayoutBuilder এর মাধ্যমে একাকটা স্ক্রিন এর জন্য একাকটা ডিজাইন করা জাই
         builder: (BuildContext context, BoxConstraints constraints) {
           if (constraints.maxWidth < 400) {
             return const Center(child: Text('Mobile'));
@@ -76,6 +80,26 @@ class Home extends StatelessWidget {
           return const Center(
             child: Text('Desktop'),
           );
+        },
+      ),
+     */
+      body: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          if (orientation == Orientation.landscape) {
+            //এটা landscape বা ফোন বাকিয়ে ধরলে
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.yellow,
+            );
+          } else {
+            return Container(
+              //এটা portrait বা ফোন সোজা ধরলে
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.red,
+            );
+          }
         },
       ),
     );
