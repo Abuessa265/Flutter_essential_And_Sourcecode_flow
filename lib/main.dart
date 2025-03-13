1e974172e85f231fe27de6bb580c070212fec02f
Abuessa265 <abuessastudent1999@gmail.com>
2025-03-13 21:29:35 +0600

    update form validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,184 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-main() {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.blue),
-        darkTheme: ThemeData(primarySwatch: Colors.amber),
-        color: Colors.blue,
-        home: const HomeActivity());
+    return MaterialApp(home: RegForm());
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class RegForm extends StatefulWidget {
+  const RegForm({super.key});
+
+  @override
+  State<RegForm> createState() => _RegFormState();
+}
+
+class _RegFormState extends State<RegForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  bool _obscurePassword = true;
+  String _userData = "";
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _phoneController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    ButtonStyle buttonStyle=ElevatedButton.styleFrom(
-        minimumSize: Size(double.infinity, 60)
-    );
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Form',
-          style: TextStyle(
-              color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.bold),
-        ),
         backgroundColor: Colors.green,
+        title: Text('Registration Form'),
+        centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(padding: EdgeInsets.all(20),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'First Name',)),),
-          Padding(padding: EdgeInsets.all(20),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Last Name',)),),
-          Padding(padding: EdgeInsets.all(20),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Email address',)),),
-          Padding(padding: EdgeInsets.all(20),child:  ElevatedButton(onPressed: (){},child: Text('Submit'),),)
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Enter name',
+                  icon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent),
+                    //Default border color below code
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.yellow),
+                    // Border color when focused below code
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.purple),
+                    //Border color when enabled but not focused below code
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.lightGreenAccent,
+                    ), // Border color when there is an error
+                  ),
+                  focusedErrorBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ), // Border color when there is an error and focused
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter email',
+                  icon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Entar your email address';
+                  }
+                  if (!RegExp(
+                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                  ).hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter password',
+                  icon: Icon(Icons.lock),
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: _obscurePassword,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 charactors';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _phoneController,
+                decoration: InputDecoration(
+                  labelText: 'Phone number',
+                  hintText: 'Enter Number',
+                  icon: Icon(Icons.phone),
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please enter your phone number';
+                  }
+                  if (value.length != 11) {
+                    return 'Phone number must be 11 digits';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(() {
+                      _userData =
+                          "Name : ${_nameController.text}\nEmail:  ${_emailController.text}\nPhone Number : ${_phoneController.text}";
+                    });
+                  }
+                },
+                child: Text('Register'),
+              ),
+              SizedBox(height: 32),
+              Text(_userData),
+            ],
+          ),
+        ),
       ),
     );
   }
